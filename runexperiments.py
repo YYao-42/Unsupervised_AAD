3a5975315a17bcbb28dece07f4845e4cda81f691
Yuanyuan Yao <yuanyuan.yao@student.kuleuven.be>
2025-08-25 15:44:46 +0200

    Script to run experiments

--- runexperiments.py
+++ runexperiments.py
@@ -1,62 +1,21 @@
 import subprocess
 from multiprocessing import Pool
 
-# script_name = 'speech_iter.py'
-# experiments = [
-#     {'dataset': 'Neetha', 'nbtraintrials': 10, 'predtriallen': 20, 'shuffle': True},
-#     {'dataset': 'Neetha', 'nbtraintrials': 20, 'predtriallen': 20, 'shuffle': True},
-#     {'dataset': 'Neetha', 'nbtraintrials': 30, 'predtriallen': 20, 'shuffle': True},
-#     {'dataset': 'Neetha', 'nbtraintrials': 40, 'predtriallen': 20, 'shuffle': True},
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 10},
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 20},
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 30},
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 40},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 1, 'predtriallen': 30, 'updatewinlen': 60},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 2, 'predtriallen': 30, 'updatewinlen': 60},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 3, 'predtriallen': 30, 'updatewinlen': 60},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 4, 'predtriallen': 30, 'updatewinlen': 60},
-# ]
-
-# script_name = 'unsupervised_single_enc.py'
-# experiments = [
-#     {'flippct': 1.0, 'bootstrap': True, 'svad': True, 'lwcov': True, 'mixpair': False, 'randinit': False},
-#     {'flippct': 1.0, 'bootstrap': True, 'svad': True, 'lwcov': True, 'mixpair': True, 'randinit': False},
-#     {'flippct': 1.0, 'bootstrap': True, 'svad': True, 'lwcov': True, 'mixpair': False, 'randinit': True},
-#     {'flippct': 1.0, 'bootstrap': True, 'svad': True, 'lwcov': True, 'mixpair': True, 'randinit': True},
-# ]
-
-# script_name = 'unsupervised_speech.py'
-# experiments = [
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 20, 'lwcov': True, 'flippct': 1.0, 'randinit': False},
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 20, 'lwcov': True, 'flippct': 1.0, 'randinit': True},
-#     # {'dataset': 'fuglsang2018', 'nbtraintrials': 20, 'lwcov': True, 'flippct': 1.0, 'randinit': True, 'unbiased': True},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 2, 'lwcov': True, 'flippct': 1.0, 'randinit': False},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 1, 'lwcov': True, 'flippct': 1.0, 'randinit': True},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 2, 'lwcov': True, 'flippct': 1.0, 'randinit': True},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 1, 'lwcov': True, 'flippct': 1.0, 'randinit': True, 'unbiased': True},
-#     # {'dataset': 'earEEG', 'nbtraintrials': 2, 'lwcov': True, 'flippct': 1.0, 'randinit': True, 'unbiased': True},
-#     # {'dataset': 'Neetha', 'nbtraintrials': 20, 'lwcov': True, 'flippct': 1.0, 'randinit': False},
-#     # {'dataset': 'Neetha', 'nbtraintrials': 20, 'lwcov': True, 'flippct': 1.0, 'randinit': True},
-#     # {'dataset': 'Neetha', 'nbtraintrials': 20, 'lwcov': True, 'flippct': 1.0, 'randinit': True, 'unbiased': True},
-
-#     {'dataset': 'Neetha', 'folds': 3, 'slidingwin': True, 'compete_resolu': 30, 'hparadata': [6,5], 'hparafeats': [1,0], 'evalpara': [1,1], 'poolsize': 19, 'initwtrainedmodel': True},
-#     {'dataset': 'Neetha', 'folds': 3, 'slidingwin': True, 'compete_resolu': 30, 'hparadata': [6,5], 'hparafeats': [1,0], 'evalpara': [1,1], 'poolsize': 19},
-#     {'dataset': 'Neetha', 'folds': 3, 'recursive': True, 'compete_resolu': 30, 'hparadata': [6,5], 'hparafeats': [1,0], 'evalpara': [1,1], 'weightpara': [0.9, 0.9], 'initwtrainedmodel': True},
-#     {'dataset': 'Neetha', 'folds': 3, 'recursive': True, 'compete_resolu': 30, 'hparadata': [6,5], 'hparafeats': [1,0], 'evalpara': [1,1], 'weightpara': [0.9, 0.9]},
-#     # {'dataset': 'fuglsang2018', 'folds': 5, 'slidingwin': True, 'compete_resolu': 25, 'hparadata': [6,5], 'hparafeats': [9,0], 'evalpara': [3,2], 'poolsize': 23, 'seeds': [2]},
-#     # {'dataset': 'fuglsang2018', 'folds': 5, 'recursive': True, 'compete_resolu': 25, 'hparadata': [6,5], 'hparafeats': [9,0], 'evalpara': [3,2], 'weightpara': [0.916, 0.916], 'seeds': [2]},
-#     # {'dataset': 'earEEG', 'folds': 5, 'slidingwin': True, 'compete_resolu': 30, 'hparadata': [4,3], 'hparafeats': [6,0], 'evalpara': [3,2], 'poolsize': 19, 'seeds': [2]},
-#     # {'dataset': 'earEEG', 'folds': 5, 'recursive': True, 'compete_resolu': 30, 'hparadata': [4,3], 'hparafeats': [6,0], 'evalpara': [3,2], 'weightpara': [0.9, 0.9], 'seeds': [2]},
-# ]
-
-script_name = 'speech_stream.py'
+script_name = 'speech_iter.py'
 experiments = [
-    {'dataset': 'Neetha', 'paratrans': True, 'method': 'all', 'predtriallen': 30, 'updatestep': 1},
-    {'dataset': 'Neetha', 'paratrans': True, 'method': 'all', 'predtriallen': 15, 'updatestep': 2},
-    {'dataset': 'fuglsang2018', 'paratrans': True, 'method': 'all', 'predtriallen': 25, 'updatestep': 1},
-    {'dataset': 'earEEG', 'paratrans': True, 'method': 'all', 'predtriallen': 30, 'updatestep': 1},
-    {'dataset': 'earEEG', 'paratrans': True, 'method': 'all', 'predtriallen': 15, 'updatestep': 2},
+    {'dataset': 'Neetha', 'nbtraintrials': 40, 'method': 'single', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 40, 'method': 'single_warminit', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 40, 'method': 'two', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 40, 'method': 'bpsk_local', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 40, 'method': 'single_unbiased', 'shuffle': True},
+
+    {'dataset': 'Neetha', 'nbtraintrials': 45, 'method': 'single', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 45, 'method': 'single_warminit', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 45, 'method': 'two', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 45, 'method': 'bpsk_local', 'shuffle': True},
+    {'dataset': 'Neetha', 'nbtraintrials': 45, 'method': 'single_unbiased', 'shuffle': True},
 ]
+
 
 # Function to run a single experiment
 def run_experiment(arg_set):
